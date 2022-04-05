bc4e036c0273247655b7aee275bcf20c74ef4e39
Akshaey Singh Thakur <64556767+AkshaeySinghThakur@users.noreply.github.com>
2022-04-05 09:41:11 +0530

    Delete cc.js

--- cc.js
+++
@@ -1,9 +0,0 @@
-var box = document.getElementById('box');
-var click_count = document.getElementById('click_count');
-var count=0;
-box.addEventListener('click' ,function(){
-count++;
-click_count.innerText=count;
-})
-
-// The Click in 4th Line represents Event will activate when clicked on the Box 
